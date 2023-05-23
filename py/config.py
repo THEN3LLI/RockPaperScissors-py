949924d510b92abe4c6d458316facd1ca51afb90
then3lli <danielecinellidev@gmail.com>
2023-05-23 20:08:36 +0200

    first commit

---
+++ py/config.py
@@ -0,0 +1,10 @@
+
+# 16:9 aspect ratio
+SCREENSIZE_X = [640, 1280, 1920, 2560]
+SCREENSIZE_Y = [480, 720, 1080, 1440]
+
+RESIZABLE = False
+FULLSCREEN = False
+
+GAMETITLE = "Rock Paper Scissors - But in Py"
+
